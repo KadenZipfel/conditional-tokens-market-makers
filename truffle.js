e1c4996f74528fe4628496e778c5416cc2623d41
Kaden Zipfel <kaden.zipfel@hotmail.com>
2021-05-19 14:10:37 -0700

    Update truffle config to include rinkeby support

--- truffle.js
+++ truffle.js
@@ -1,73 +1,79 @@
 require("chai/register-should");
-require('mocha-steps')
+require("mocha-steps");
+const HDWalletProvider = require("truffle-hdwallet-provider");
 
 const config = {
-    networks: {
-        mainnet: {
-            host: "localhost",
-            port: 8545,
-            network_id: "1",
-        },
-        ropsten: {
-            host: "localhost",
-            port: 8545,
-            network_id: "3",
-        },
-        kovan: {
-            host: "localhost",
-            port: 8545,
-            network_id: "42",
-        },
-        rinkeby: {
-            host: "localhost",
-            port: 8545,
-            network_id: "4",
-        },
-        goerli: {
-            host: "localhost",
-            port: 8545,
-            network_id: "5",
-        },
-        develop: {
-            host: "localhost",
-            port: 8545,
-            network_id: "*",
-	},
+  networks: {
+    mainnet: {
+      host: "localhost",
+      port: 8545,
+      network_id: "1",
     },
-    mocha: {
-        enableTimeouts: false,
-        grep: process.env.TEST_GREP,
-        reporter: "eth-gas-reporter",
-        reporterOptions: {
-            currency: "USD",
-            excludeContracts: ["Migrations"]
-        }
+    ropsten: {
+      host: "localhost",
+      port: 8545,
+      network_id: "3",
     },
-    compilers: {
-        solc: {
-            version: "0.5.10",
-            settings: {
-                optimizer: {
-                    enabled: true
-                }
-            }
-        }
-    }
-}
+    kovan: {
+      host: "localhost",
+      port: 8545,
+      network_id: "42",
+    },
+    rinkeby: {
+      //   host: "localhost",
+      //   port: 8545,
+      provider: function () {
+        return new HDWalletProvider(
+          process.env.mnemonic,
+          `https://rinkeby.infura.io/v3/${process.env.infuraId}`
+        );
+      },
+      network_id: "4",
+    },
+    goerli: {
+      host: "localhost",
+      port: 8545,
+      network_id: "5",
+    },
+    develop: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*",
+    },
+  },
+  mocha: {
+    enableTimeouts: false,
+    grep: process.env.TEST_GREP,
+    reporter: "eth-gas-reporter",
+    reporterOptions: {
+      currency: "USD",
+      excludeContracts: ["Migrations"],
+    },
+  },
+  compilers: {
+    solc: {
+      version: "0.5.10",
+      settings: {
+        optimizer: {
+          enabled: true,
+        },
+      },
+    },
+  },
+};
 
-const _ = require('lodash')
+const _ = require("lodash");
 
 try {
-    _.merge(config, require('./truffle-local'))
-}
-catch(e) {
-    if(e.code === 'MODULE_NOT_FOUND') {
-        // eslint-disable-next-line no-console
-        console.log('No local truffle config found. Using all defaults...')
-    } else {
-        // eslint-disable-next-line no-console
-        console.warn('Tried processing local config but got error:', e)
-    }
+  _.merge(config, require("./truffle-local"));
+} catch (e) {
+  if (e.code === "MODULE_NOT_FOUND") {
+    // eslint-disable-next-line no-console
+    console.log("No local truffle config found. Using all defaults...");
+  } else {
+    // eslint-disable-next-line no-console
+    console.warn("Tried processing local config but got error:", e);
+  }
 }
 
-module.exports = config
+module.exports = config;
