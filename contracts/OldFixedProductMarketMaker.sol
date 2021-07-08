3ce6e55d4d1cdea49123419bc733b43b2ef0b38a
Kaden Zipfel <kaden.zipfel@hotmail.com>
2021-07-08 14:48:09 -0700

    fix: use old erc20 contract for old fpmm contract

--- contracts/OldFixedProductMarketMaker.sol
+++ contracts/OldFixedProductMarketMaker.sol
@@ -5,7 +5,7 @@ import { IERC20 } from "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import { ConditionalTokens } from "@gnosis.pm/conditional-tokens-contracts/contracts/ConditionalTokens.sol";
 import { CTHelpers } from "@gnosis.pm/conditional-tokens-contracts/contracts/CTHelpers.sol";
 import { ERC1155TokenReceiver } from "@gnosis.pm/conditional-tokens-contracts/contracts/ERC1155/ERC1155TokenReceiver.sol";
-import { ERC20 } from "./ERC20.sol";
+import { ERC20 } from "./OldERC20.sol";
 
 
 library CeilDiv {
